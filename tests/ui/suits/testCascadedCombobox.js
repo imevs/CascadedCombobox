6861e2b17b56c6c91b133ad852f4f27841fe9e59
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-05-27 01:44:00 +0400

    CascadedCombobox-1 add dependency between comboboxes (tests)

--- tests/ui/suits/testCascadedCombobox.js
+++ tests/ui/suits/testCascadedCombobox.js
@@ -5,13 +5,56 @@ StartTest(function(t) {
             waitFor: 1000
         },
         function(next) {
-            var cmb = t.cq1('cascadedcombobox > combobox');
-            t.click(cmb.el.query('.x-trigger-cell')[ 0 ]);
-            t.click(cmb.getPicker().getNode(0), next);
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCity = comboboxes[1];
 
-            //t.ok(false);
+            t.is(cmbCity.getPicker().getNodes().length, 0);
 
             next();
+        },
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCountry = comboboxes[0];
+
+            // select first country
+            t.click(cmbCountry.el.query('.x-trigger-cell')[ 0 ]);
+            t.click(cmbCountry.getPicker().getNode(0), next);
+        },
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCity = comboboxes[1];
+            t.click(cmbCity.el.query('.x-trigger-cell')[ 0 ], next);
+        },
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCity = comboboxes[1];
+
+            t.is(cmbCity.getPicker().getNodes().length, 1);
+            t.is(cmbCity.getPicker().getNode(0).textContent, 'Moscow');
+
+            next();
+        },
+
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCountry = comboboxes[0];
+
+            // select first country
+            t.click(cmbCountry.el.query('.x-trigger-cell')[ 0 ], next);
+        },
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCountry = comboboxes[0];
+
+            t.click(cmbCountry.getPicker().getNode(1), next);
+        },
+        function(next) {
+            var comboboxes = t.cq('cascadedcombobox');
+            var cmbCity = comboboxes[1];
+            t.click(cmbCity.el.query('.x-trigger-cell')[ 0 ]);
+
+            t.is(cmbCity.getPicker().getNodes().length, 1);
+            t.is(cmbCity.getPicker().getNode(0).textContent, 'Orsk');
         }
 
     );
