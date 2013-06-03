64f9dea4b2599a2718956b50d4830ffbfb3ed1aa
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-06-04 01:04:58 +0400

    [CascadedCombobox-2] save selected values in filters in cookies (debug tests)

--- tests/ui/suits/testAutoDiscover.js
+++ tests/ui/suits/testAutoDiscover.js
@@ -5,6 +5,7 @@ StartTest(function(t) {
             waitFor: 1000
         },
         function(next) {
+            console.log('1');
             var comboboxes = t.cq('cascadedcombobox');
             var cmbCountry = comboboxes[0];
             var cmbCity = comboboxes[1];
@@ -16,6 +17,7 @@ StartTest(function(t) {
         },
         // select England
         function(next) {
+            console.log('2');
             var comboboxes = t.cq('cascadedcombobox');
             var cmbCountry = comboboxes[0];
 
@@ -23,12 +25,14 @@ StartTest(function(t) {
             t.click(cmbCountry.getPicker().getNode(2), next);
         },
         function(next) {
+            console.log('3');
             var comboboxes = t.cq('cascadedcombobox');
             var cmbCountry = comboboxes[0];
             var cmbCity = comboboxes[1];
 
             t.is(cmbCountry.getRawValue(), 'England');
             t.is(cmbCity.getRawValue(), 'London');
+            console.log('4');
         }
     );
 
