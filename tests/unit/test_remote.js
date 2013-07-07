cc96018471d6076b0ef8bc4a53706a83e1fe51f9
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-07-07 14:45:08 +0400

    [CascadedCombobox-7] Make Kladr demo (add test)

--- tests/unit/test_remote.js
+++ tests/unit/test_remote.js
@@ -1,16 +1,31 @@
 AsyncTestCase("Kladr testCase", {
     setUp: function () {
+        Ext.util.Cookies.clear('region');
+        Ext.util.Cookies.clear('district');
+        Ext.create('IMEVS.kladr.Region');
+        Ext.create('IMEVS.kladr.District');
     },
-    tearDown: function() {
-    },
-    testA: function (q) {
-        var cmb = Ext.create('IMEVS.kladr.Region', {});
-        var cmb2 = Ext.create('IMEVS.kladr.District', {});
-        cmb.store.load();
+    testEmptyCookie: function(q) {
+        expectAsserts(2);
+
+        Ext.ComponentQuery.query('#region')[0].store.load();
 
         q.call(function(callbacks){
             var myCallback = callbacks.add(function() {
-                console.log('myCallback');
+                assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
+                assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 0);
+            });
+            window.setTimeout(myCallback, 1000);
+        });
+    },
+    testGetRegionValueFromCookie: function (q) {
+        expectAsserts(2);
+
+        Ext.util.Cookies.set('region', 'Оренбургская');
+        Ext.ComponentQuery.query('#region')[0].store.load();
+
+        q.call(function(callbacks){
+            var myCallback = callbacks.add(function() {
                 assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
                 assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 35);
             });
