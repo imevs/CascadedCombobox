698ecf249c9151673fc99d9c8a32c5d7860051ad
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-07-09 23:17:09 +0400

    [CascadedCombobox-3] fill values from request GET parameteres (fix tests, mock JsonP)

--- tests/unit/test_remote.js
+++ tests/unit/test_remote.js
@@ -1,5 +1,17 @@
-AsyncTestCase("Kladr testCase", {
+TestCase("Kladr testCase", {
+    getFixtureData: function (testFile) {
+        return jQuery.ajax({
+            type: "GET",
+            url: location.protocol + '//' + location.host + '/test/tests/unit/data_' + testFile + '.json',
+            data: '',
+            async:  false
+        }).responseText;
+    },
+
     setUp: function () {
+        this.regions = JSON.parse(this.getFixtureData('regions'));
+        this.districts = JSON.parse(this.getFixtureData('districts'));
+
         Ext.data.JsonP.requestCount = 0;
 
         Ext.util.Cookies.clear('region');
@@ -9,31 +21,20 @@ AsyncTestCase("Kladr testCase", {
         Ext.create('IMEVS.kladr.District');
     },
     testEmptyCookie: function(q) {
-        expectAsserts(3);
+        Ext.data.JsonP.callback1(this.regions);
 
-        q.call(function(callbacks){
-            var myCallback = callbacks.add(function() {
-                assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
-                assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 0);
-
-                assertEquals(Ext.data.JsonP.requestCount, 1);
-            });
-            window.setTimeout(myCallback, 1000);
-        });
+        assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
+        assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 0);
+        assertEquals(Ext.data.JsonP.requestCount, 1);
     },
     testGetRegionValueFromCookie: function (q) {
-        expectAsserts(3);
-
         Ext.util.Cookies.set('region', 'Оренбургская');
 
-        q.call(function(callbacks){
-            var myCallback = callbacks.add(function() {
-                assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
-                assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 35);
+        Ext.data.JsonP.callback1(this.regions);
+        Ext.data.JsonP.callback2(this.districts);
 
-                assertEquals(Ext.data.JsonP.requestCount, 2);
-            });
-            window.setTimeout(myCallback, 2000);
-        });
+        assertEquals(Ext.ComponentQuery.query('#region')[0].store.data.items.length, 96);
+        assertEquals(Ext.ComponentQuery.query('#district')[0].store.data.items.length, 35);
+        assertEquals(Ext.data.JsonP.requestCount, 2);
     }
 });
